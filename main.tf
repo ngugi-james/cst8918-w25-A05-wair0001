1785b5a000c2546692ca5716effe6ca7b21c8137
ngugi-james <ngugijamesw@gmail.com>
2025-02-07 23:13:34 -0500

    Link security group to the NIC

--- main.tf
+++ main.tf
@@ -117,3 +117,21 @@ resource "azurerm_network_interface" "webserver" {
   }
 }
 
+# Link the security group to the NIC
+resource "azurerm_network_interface_security_group_association" "webserver" {
+  network_interface_id      = azurerm_network_interface.webserver.id
+  network_security_group_id = azurerm_network_security_group.webserver.id
+}
+
+# Define the init script template
+data "cloudinit_config" "init" {
+  gzip          = false
+  base64_encode = true
+
+  part {
+    filename     = "init.sh"
+    content_type = "text/x-shellscript"
+
+    content = file("${path.module}/init.sh")
+  }
+}
