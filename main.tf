d3bd48789bda882fa51ea676e2323213992fb352
ngugi-james <ngugijamesw@gmail.com>
2025-02-07 23:11:57 -0500

    define resource group, public IP address and virtual network

--- main.tf
+++ main.tf
@@ -41,3 +41,26 @@ variable "admin_username" {
   default     = "azureadmin"
   description = "This is the username to be used by the administrator."
 }
+
+# Define the resource group
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.labelPrefix}-A05-RG"
+  location = var.region
+}
+
+# Define a public IP address
+resource "azurerm_public_ip" "webserver" {
+  name                = "${var.labelPrefix}A05PublicIP"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+}
+
+# Define the virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.labelPrefix}A05Vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
